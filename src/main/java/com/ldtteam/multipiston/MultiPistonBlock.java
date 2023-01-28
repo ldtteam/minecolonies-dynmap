ac5a3f3f6e0f00ce55b1ebd6fff7fe64ac6f090a
Raycoms <ray.neiheiser@gmail.com>
2023-01-28 17:52:08 +0100

    fix 421

--- src/main/java/com/ldtteam/multipiston/MultiPistonBlock.java
+++ src/main/java/com/ldtteam/multipiston/MultiPistonBlock.java
@@ -6,7 +6,6 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
@@ -79,13 +78,6 @@ public class MultiPistonBlock extends BaseEntityBlock
         {
             ((TileEntityMultiPiston) te).handleRedstone(level.hasNeighborSignal(pos));
         }
-    }
-
-    @Override
-    public BlockState rotate(final BlockState state, final LevelAccessor world, final BlockPos pos, final Rotation direction)
-    {
-        ((TileEntityMultiPiston) world.getBlockEntity(pos)).rotate(direction);
-        return super.rotate(state, world, pos, direction);
     }
 
     @Nullable
